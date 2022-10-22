1b4550fa6c8c0c9ea4e7befcdcd8f0420b1f2b9a
Abou-Bakr <chikouche.aboubakr@gmail.com>
2022-10-22 13:49:50 +0100

    Add project files.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Net.Http.Json;
+using System.Windows.Controls;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Linq;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        bool isComma { get; set; } = false;
+
+        private string _inputValue = "0";
+        public string InputValue
+        {
+            get { return _inputValue; }
+            set
+            {
+                _inputValue = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _result = "zero";
+        public string Result
+        {
+            get { return _result; }
+            set
+            {
+                _result = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var task = new Task(async () =>
+                {
+                    using (var client = new HttpClient())
+                    {
+                        //Post Data
+                        client.BaseAddress = new Uri("https://localhost:7009/");
+                        client.DefaultRequestHeaders.Accept.Clear();
+                        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                        var response = await client.PostAsJsonAsync("api/convertor/", InputValue);
+                        response.EnsureSuccessStatusCode();
+
+                        //Fetch Data
+                        Result = await client.GetStringAsync("api/convertor");
+                        Result = Result.ToString();
+                    }
+                });
+
+                task.Start();
+                await task;
+                task.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            ConvertButton.IsEnabled = true;
+
+            Regex regex = new Regex("[^0-9.-]+");
+            var value = e.Text;
+            e.Handled = regex.IsMatch(value);
+
+            if (Regex.IsMatch(InputValue, @"\.\d\d"))
+            {
+                e.Handled = true;
+                isComma = true;
+            }
+        }
+
+        private void TextValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (isComma && InputValue.Length > 12 || InputValue.Length > 9)
+            {
+                MessageBox.Show("Input to long");
+                ConvertButton.IsEnabled = false;
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Server/Controllers/ConvertorController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConvertorController : ControllerBase
+    {
+        private readonly ILogger<ConvertorController> _logger;
+        private static string? finalResult { get; set; }
+
+        [HttpGet]
+        public string Get()
+        {
+            return finalResult;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult Post([FromBody] string value)
+        {
+            try
+            {
+                finalResult = ConvertToWords(value.ToCharArray());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.UtcNow);
+                _logger.LogInformation("Exception", ex.Message);
+            }
+            return NoContent();
+        }
+
+        private string ConvertToWords(char[] input)
+        {
+            string? firstPartResult = null;
+            string? finalResult = null;
+            string? sum = null;
+            var space = " ";
+            var singlesUnits = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+            var tensUnites = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+            var found = input.Where(o => o.Equals('.')).ToString();
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                var separator = input[i];
+                if (separator == '.')
+                {
+                    string part;
+                    if (input.Length >= i + 2) part = "0";
+                    else part = input[i + 2].ToString();
+
+                    var separatorPart = input[i + 1].ToString() + part;
+                    var value = int.Parse(separatorPart.ToString());
+                    if (value < 19)
+                    {
+                        firstPartResult = singlesUnits[value];
+                    }
+                    else if (value >= 20)
+                    {
+                        int? firstPart = value / 10;
+                        int? secoundPart = value - firstPart * 10;
+
+                        firstPartResult += tensUnites[(int)firstPart] + space;
+                        if (secoundPart > 0 && secoundPart <= 9) firstPartResult += singlesUnits[(int)secoundPart] + space;
+                        firstPartResult += "cent";
+                    }
+                    sum = null;
+                }
+                else
+                {
+                    sum = input[i].ToString() + sum;
+                }
+            }
+            if (sum != null)
+            {
+                var value = int.Parse(sum.ToString());
+                for (var j = 1000000000; j >= 1; j /= 1000)
+                {
+                    int hundreds = 0, tens = 0, ones = 0;
+                    int result = value / j;
+                    if (result != 0)
+                    {
+                        hundreds = result / 100;
+                        tens = (result - hundreds * 100) / 10;
+                        ones = result - hundreds * 100 - tens * 10;
+
+                        if (ones + tens * 10 < 20)
+                        {
+                            finalResult += singlesUnits[hundreds] + " hunderds and " + singlesUnits[ones + tens * 10];
+                        }
+                        else
+                        {
+                            finalResult += singlesUnits[hundreds] + " hunderds and " + tensUnites[tens] + space + singlesUnits[ones];
+                        }
+
+                        switch (j)
+                        {
+                            case 1000000000:
+                                finalResult += " billion ";
+                                break;
+                            case 1000000:
+                                finalResult += " million ";
+                                break;
+                            case 1000:
+                                finalResult += " thousands ";
+                                break;
+                        }
+                    }
+                    value = value - ((hundreds * 100 + tens * 10 + ones) * j);
+                }
+            }
+
+            if (string.IsNullOrEmpty(firstPartResult)) return finalResult;
+            if (string.IsNullOrEmpty(finalResult)) return firstPartResult;
+            return finalResult + space + "and " + firstPartResult;
+        }
+    }
+}
